4a9cca25d7fbca97de1ede28d91a5ae4dee051e0
Anagas <anagassatheeshkumar@gmail.com>
2024-07-04 18:48:06 +0530

    Add cart,product page and functionality

---
+++ src/app/cart/page.jsx
@@ -0,0 +1,113 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { addToCart, removeFromCart } from "@/redux/slices/cartSlice";
+
+export default function CartPage() {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const { loading, cartItems, itemsPrice } = useSelector((state) => state.cart);
+
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id));
+  };
+  const addToCartHandler = async (product, qty) => {
+    dispatch(addToCart({ ...product, qty }));
+  };
+
+  return (
+    <div>
+      <h1 className="mb-4 text-xl"> Cart</h1>
+      {loading ? (
+        <div>Loading...</div>
+      ) : cartItems.length === 0 ? (
+        <div>
+          Cart is Empty.<Link href="/">Add items</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">
+            <table className="min-w-full">
+              <thead>
+                <tr>
+                  <th className="p-5 text-left">Product</th>
+                  <th className="p-5 text-right">Quantity</th>
+                  <th className="p-5 text-right">Price</th>
+                  <th className="p-5">Remove</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map((item) => (
+                  <tr key={item.id} className="border-b">
+                    <td>
+                      <Link
+                        href={`/product/${item.id}`}
+                        className="flex items-center"
+                      >
+                        <Image
+                          src="/assets/HomePage/trophie.png"
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                          className="p-1"
+                        />
+                        {item.name}
+                      </Link>
+                    </td>
+                    <td className="p-5 text-right">
+                      <select
+                        value={item.qty}
+                        onChange={(e) =>
+                          addToCartHandler(item, Number(e.target.value))
+                        }
+                      >
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </select>
+                    </td>
+                    <td className="p-5 text-right">₹ {item.price}</td>
+                    <td className="p-5 text-center">
+                      <button
+                        className="default-button"
+                        onClick={() => removeFromCartHandler(item.id)}
+                      >
+                        X
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div>
+            <div className="card p-5">
+              <ul>
+                <li>
+                  <div className="pb-3 text-xl">
+                    Subtotal ({cartItems.reduce((a, c) => a + c.qty, 0)}): ₹
+                    {itemsPrice}
+                  </div>
+                </li>
+                <li>
+                  <button
+                    className="primary-button w-full"
+                    onClick={() => router.push("/shipping")}
+                  >
+                    Proceed to checkout
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "@/components/Navbar";
+import { StoreProvider } from "@/redux/StoreProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,11 +15,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main>
-          <Navbar />
+        <StoreProvider>
+          <main>
+            <Navbar />
 
-          {children}
-        </main>
+            {children}
+          </main>
+        </StoreProvider>
       </body>
     </html>
   );
