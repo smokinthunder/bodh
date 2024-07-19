2c1fd3c657a2ce8b2cd63299145fbcf1908d8a2b
Antesh <anteshkumar2003@gmail.com>
2024-07-19 21:08:12 +0530

    Added not found Page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,10 +1,10 @@
 export default function NotFound() {
-  return (
-    <div className="flex justify-center items-center h-[100vh]">
-      <img
-        src="/assets/Error Page/404-error-page-C84qTQWgXI.png"
-        className="w-1/2 max-w-80"
-      />
-    </div>
-  );
-}
+    return (
+      <div className="flex items-center justify-center h-[calc(100vh-96px)]">
+        <img
+          src="/assets/Error Page/404-error-page-C84qTQWgXI.png"
+          className="w-1/2"
+        />
+      </div>
+    );
+  }  
