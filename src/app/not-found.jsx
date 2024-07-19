8c8416fe590d0326f8d03e2954d62f15ddf2b09c
Antesh <anteshkumar2003@gmail.com>
2024-07-19 19:51:00 +0530

    404 page created

---
+++ src/app/not-found.jsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <img
+        src="/assets/Error Page/404-error-page-C84qTQWgXI.png"
+        className="w-1/2 max-w-80"
+      />
+    </div>
+  );
+}
